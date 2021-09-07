4c75a9670cf49e976cb735bc77e6d6bd706e492c
Caleb Martin <caleb637@icloud.com>
2021-09-07 18:30:33 -0400

    design for home completed

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import '../css/Footer.css'
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p className='footer__text'>
+        © 2021 PA Dutch Builders<p className='footer__r'>®</p>, LLC | hosted by
+        Destefano Enterprises, LLC
+      </p>
+      <p className='footer__text'>
+        PA Dutch Builders<p className='footer__r'>®</p> is a registered mark of
+        the US patent and trademark office, all rights reserved.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import '../css/Home.css'
 
 import ShowcaseProduct from '../Components/ShowcaseProduct'
+import Footer from '../Components/Footer'
 
 function Home() {
   return (
@@ -118,6 +119,68 @@ function Home() {
           <p className='interested__email'>Sales@padutchbuilders.com</p>
         </div>
       </div>
+      <div className='home__aboutamish'>
+        <h2 className='aboutamish__title'>A Little About the Amish</h2>
+        <p className='aboutamish__subtitle'>
+          Welcome, we have been working with the Amish for over 23 years.
+        </p>
+        <p className='aboutamish__para'>
+          The farmlands of the Pennsylvania Dutch Country are among the most
+          productive in the nation. But many of the farmers here are different
+          from most Americans; different by choice. For these are the Old Order
+          Amish and Mennonites, also known as the "Plain People". <br />
+          <br />
+          Our Amish neighbors have been employing horse-drawn power since the
+          days when horsepower had a whole different meaning! In comparison to
+          our fast-paced society, the simpler, family-centered Amish way of life
+          holds a special fascination. <br />
+          <br />
+          These people trace their heritage back hundreds of years, and yet,
+          despite all the time that has passed and the many changes that have
+          taken place in society, they still live and work much as their
+          forefathers did. Their families and their farms are their top
+          priorities, second only to God. <br />
+          <br />
+          The Amish are very devout in their faith. They believe in the literal
+          interpretation and application of Scripture as the Word of God. They
+          take seriously the Biblical commands to separate themselves from the
+          things of the world. They believe worldliness can keep them from being
+          close to God, and can introduce influences that could be destructive
+          to their communities and to their way of life. <br />
+          <br />
+          Today there are over 25 different Amish, Mennonite, and Brethren
+          church groups in Lancaster County, all holding to slightly different
+          traditions and their own interpretations of the Bible. The more
+          traditional groups are called 'old order'. They do not permit
+          electricity or telephones in their homes. By restricting access to
+          television, radio, and telephones, the Amish are better able to keep
+          the modern world from intruding into their home life. <br />
+          <br />
+          The Amish have long preferred farming as a way of life. They feel
+          their lifestyle and their families can best be maintained in a rural
+          environment. While they do not permit the use of tractors in their
+          fields, these old order Amish groups do use modern farm equipment
+          pulled by teams of horses or mules. Many of the Amish are also expert
+          wood workers and have had their craft passed down form generations of
+          craftsmen. <br />
+          <br />
+          These traditional groups wear plain clothing styles, which has earned
+          them the name "Plain People". It is the simple, peaceful lifestyle of
+          these plain people that attracts such a curiosity today. Many wonder
+          how these people can survive in their supposedly backward ways. Well,
+          they're not only surviving - they're thriving. Since 1960, the Amish
+          population in Lancaster County has almost tripled.
+        </p>
+        <img
+          className='aboutamish__banner'
+          src='/public/images/aboutamishimg.png'
+          alt='About the Amish Banner'
+        />
+        <h2 className='aboutamish__cta'>
+          ...let our connection be your connection!
+        </h2>
+      </div>
+      <Footer />
     </div>
   )
 }
